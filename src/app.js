0f3706b68692763e0fd82443623ab8f6d2b7cbb0
ViktorBash <basharkevichv@gmail.com>
2020-06-22 00:47:50 -0400

    Added localstorage support so items will save even after closing the tab

--- src/app.js
+++ src/app.js
@@ -1,6 +1,28 @@
+// Get some elements from the HTML for the functions later on
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
 var filter = document.getElementById('filter');  // For search functionality
+
+// Load in previous items from localStorage
+function loadItems() {
+    Object.keys(localStorage).forEach(function (key) {
+        let loadedText = localStorage.getItem(key);
+
+        // Create the list element and later on the deleteButton that will be a child of it
+        let li = document.createElement('li');
+        li.className = 'list-group-item';
+
+        let deleteButton = document.createElement('button');
+        deleteButton.className = 'btn btn-danger btn-sm float-right delete';
+        deleteButton.appendChild(document.createTextNode('Delete'));
+
+        // Add it to the itemList to get it onto the HTML
+        li.appendChild(document.createTextNode(loadedText));
+        li.appendChild(deleteButton);
+        itemList.append(li)
+    });
+}
+loadItems();
 
 // Form submit event
 form.addEventListener('submit', addItem);
@@ -12,54 +34,54 @@ filter.addEventListener('keyup', filterItems);
 function addItem(e) {
     e.preventDefault();  // Stop the normal submission of the form from happening
 
-    // Get input value
-    var newItem = document.getElementById('addItem').value;
-    // Create new li element
-    var li = document.createElement('li')
-    // Add class to li element
+    // Create the list item using the input from the user
+    let newItem = document.getElementById('addItem').value;
+    let li = document.createElement('li')
     li.className = "list-group-item";
-    // Add text node with input value
     li.appendChild(document.createTextNode(newItem));
 
+    // Add it to the HTML
     itemList.appendChild(li);
 
-    // Create del button element
-    var deleteButton = document.createElement('button');
-    // Add classes to button
+    // Create and add the deleteButton that goes on the list item
+    let deleteButton = document.createElement('button');
     deleteButton.className = 'btn btn-danger btn-sm float-right delete';
-    // Append text node
     deleteButton.appendChild(document.createTextNode('Delete'));
     li.appendChild(deleteButton)
 
-    // Reset the value in the input field
-    var inputField = document.getElementById('addItem')
+    // Reset the value in the input field to be blank
+    let inputField = document.getElementById('addItem')
     inputField.value = "";
 
+    // Add to local storage for next session
+    localStorage.setItem(newItem, newItem)
 }
 
 function removeItem(e) {
     if (e.target.classList.contains('delete')){
-        var li = e.target.parentElement;
+        let li = e.target.parentElement;
+        let textContent = li.firstChild;
+        console.log(textContent)
+        textContent = textContent.nodeValue;
+        console.log("hi:" + textContent)
+        localStorage.removeItem(textContent)
         itemList.removeChild(li);
     }
-
 }
 
 function filterItems(e) {
-    // Convert all text to lowercase
-    var text = e.target.value.toLowerCase();
-    var items = itemList.getElementsByTagName('li');
+    // Convert all input text to lowercase
+    let text = e.target.value.toLowerCase();
+    let items = itemList.getElementsByTagName('li');
 
+    // Sorting feature to either show or hide a list item based on if part of it matches the input
     Array.from(items).forEach(function(item) {
         let itemName = item.firstChild.textContent;
         if(itemName.toLowerCase().indexOf(text) !== -1 ) {
             item.style.display = 'block';
-
         }
         else {
             item.style.display = 'none';
         }
-
     });
-
 }
